f595f93c7e73fe520a745bf20cfcac7381f39f96
matsushita <matsushita@matsushitakazuminoMacBook-ea.local>
2018-11-14 23:04:09 +0900

    jQuery01課題

--- test01.js
+++ test01.js
@@ -1,8 +1,17 @@
 var answer = prompt('数値を入れてください');
 alert(answer);
 
-
-
+var i = prompt('数値を入れてください');
+var i = 0;
+if ( i < 0 ){
+   alert("負の数");
+}
+else if ( i > 0 ) {
+    alert("正の数");
+}
+else {
+    alert("0");
+}
 
 //3.
 var animal = ['パンダ', 'ライオン', 'キリン'];
@@ -27,20 +36,42 @@ if (answer == 'パンダ') {
 } else if (answer == 'ライオン') {
   alert('ライオンですか！');
 }
-//7.
-var animal = {'name': 'パンダ', 'weight': 80};
-alert(animal['name']);
-alert(animal['weight']);
-var animal = {'name': 'ライオン', 'weight': 200};
-alert(animal['name']);
-alert(animal['weight']);
-var animal = {'name': 'キリン', 'weight': 300};
-alert(animal['name']);
-alert(animal['weight']);
+
 //6.
-var fanimal = [
+var animal = [
   {'name': 'パンダ', 'weight': 80},  // 0番目のオブジェクト変数
   {'name': 'ライオン', 'weight': 200},  // 1番目のオブジェクト変数
   {'name': 'キリン', 'weight': 300}   // 2番目のオブジェクト変数
 ];
+alert(animal[1].name);
 alert(animal[1].weight);
+
+//7.
+var answer = prompt('動物の名前を入れてください');
+switch (answer) {
+  case 'パンダ':
+    alert('パンダは80kgです');
+    break;
+  case 'ライオン':
+    alert('ライオンは200kgです');
+    break;
+  case 'キリン':
+    alert('キリンは300kgです');
+    break;
+  default:
+    alert('パンダorライオンorキリンを入力してください');
+    break;
+}
+
+function menseki(r){
+  return 3.14 * r * r;
+}
+function enshu(r){
+  return 2 * 3.14 * r;
+}
+var r = 76;
+
+document.write("<p>");
+document.write("半径 = " + r + "<br />");
+document.write("面積 = " + menseki(r));
+document.write("</p>");
