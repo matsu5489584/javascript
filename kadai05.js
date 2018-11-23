ac5de6adad76cb53eef2a39e745076fa9d244d0b
matsushita <matsushita@matsushitakazuminoMacBook-ea.local>
2018-11-23 15:36:06 +0900

    js02 kadai

--- kadai05.js
+++ kadai05.js
@@ -3,7 +3,8 @@
     // この中に処理を書きます
     $('#index').find('li').each(function() {
         // ループ内の処理
-        alert($(this).text().length);
+      var count = $(this).text().length;
+      $(this).text($(this).text()+' [' + count  + '文字]');
       });
   });
 })(jQuery, window);
