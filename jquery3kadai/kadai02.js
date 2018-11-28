1ee51d565eb18daa19baed973a60087723f76024
matsushita <matsushita@matsushitakazuminoMacBook-ea.local>
2018-11-29 00:44:39 +0900

    js03 kadai

--- jquery3kadai/kadai02.js
+++ jquery3kadai/kadai02.js
@@ -3,8 +3,20 @@
     $('#login').on('click', function(e) {
       var email = $('#email').val(),
           password = $('#password').val();
+      // alert(email);
+      // alert(password);
       alert(email);
+      if (email == '') {
+        alert('メールアドレスを入力してください');
+      } else if (email.indexOf('@') < 0) {
+        alert('メールアドレスには必ず「@」が必要です');
+      }
       alert(password);
+      if (password == '') {
+        alert('パスワードを入力してください');
+      } else if (password.length < 6) {
+        alert('パスワードは6文字以上です');
+      }
     });
   });
 })(jQuery, window);
---
+++ jquery3kadai/kadai04.js
@@ -0,0 +1,9 @@
+(function($, window) {
+  $(function() {
+    // ここに処理を書きます
+    $('#date_picker').flatpickr({
+      showOn: "both",
+      buttonText: "カレンダーを表示"
+  });
+    });
+})(jQuery, window);
